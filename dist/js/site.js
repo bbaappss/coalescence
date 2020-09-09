165b1d2710231edfb01cd9871609045411e81d08
Brandon Scott <email@brandonandrewscott.com>
2020-09-09 14:13:50 -0700

    Animate the clouds in the header

--- dist/js/site.js
+++ dist/js/site.js
@@ -609,32 +609,33 @@ $(window).on("load", function () {
   header_p_height = $('.layer-1').height();
   $("header").height(header_p_height);
 
-  foot_p_height = $('.layer-1-foot').height();
-  $("#footerparallax").height(foot_p_height);
-
 
   $(window).resize(function () {
     header_p_height = $('.layer-1').height();
     $("header").height(header_p_height);
 
-    foot_p_height = $('.layer-1-foot').height();
-    $("#footerparallax").height(foot_p_height);
   });
 
 
   // init ScrollMagic Controller
   var headercontroller = new ScrollMagic.Controller();
 
-  // init ScrollMagic Controller
-  var footercontroller = new ScrollMagic.Controller();
-
   //header animations
+
+  var layer_five = TweenMax.to('.layer-5', 2, {
+    y: '20%',
+    ease: Power0.easeNone
+  });
 
   var layer_four = TweenMax.to('.layer-4', 2, {
     y: '-20%',
     ease: Power0.easeNone
   });
 
+  var sliding_background = TweenMax.to('.sliding-background-inner img', 20, {
+    x: '-100%',
+    ease: Power0.easeNone
+  }).repeat(-1).yoyo( true );
 
   var layer_three = TweenMax.to('.layer-3', 2, {
     y: '-10%',
@@ -643,24 +644,11 @@ $(window).on("load", function () {
 
 
   var layer_two = TweenMax.to('.layer-2', 2, {
-    y: '10%',
+    y: '-10%',
     ease: Power0.easeNone
   });
 
 
-  var layer_three_foot = TweenMax.to('.layer-2-foot', 2, {
-    y: '-4%',
-    ease: Power0.easeNone
-  });
-
-  // Scale Scene
-  var layer_one_scene = new ScrollMagic.Scene({
-      triggerElement: '#footerparallax',
-      triggerHook: 0,
-      duration: '100%',
-    })
-    .setTween(layer_three_foot)
-    .addTo(footercontroller);
 
   // Scale Scene
   var layer_one_scene = new ScrollMagic.Scene({
@@ -678,6 +666,15 @@ $(window).on("load", function () {
       duration: header_p_height
     })
     .setTween(layer_two)
+    .addTo(headercontroller);
+  // Scale Scene
+
+  var layer_five_scene = new ScrollMagic.Scene({
+      triggerElement: 'header',
+      triggerHook: 0,
+      duration: header_p_height
+    })
+    .setTween(layer_five)
     .addTo(headercontroller);
 
   // Scale Scene
