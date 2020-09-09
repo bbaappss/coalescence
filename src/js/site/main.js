ead06a0f569facf609176725a34d02ab79b36872
Brandon Scott <email@brandonandrewscott.com>
2020-09-09 12:23:18 -0700

    Fix some header jank

--- src/js/site/main.js
+++ src/js/site/main.js
@@ -117,7 +117,9 @@ const debounce = (fn) => {
 // Reads out the scroll position and stores it in the data attribute
 // so we can use it in our stylesheets
 const storeScroll = () => {
-  document.documentElement.dataset.scroll = window.scrollY;
+  if (window.scrollY >= 0) {
+    document.documentElement.dataset.scroll = window.scrollY;    
+  }
 }
 
 // Listen for new scroll events, here we debounce our `storeScroll` function
